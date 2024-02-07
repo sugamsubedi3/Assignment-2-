2884829bb316dae96f01d83d675db0a7768e92f7
sugamsubedi3 <157234324+sugamsubedi3@users.noreply.github.com>
2024-02-06 20:54:33 -0500

    Add files via upload
    this is the assignment 2 as per the given instruction.

---
+++ Interval.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Interval
+{
+    static void Main()
+    {
+        Console.Write("Enter a real number: ");
+        double x = double.Parse(Console.ReadLine());
+
+        // Test the membership of x to the set I
+        bool belongsToI = (x >= 2 && x < 3) || (x >= 0 && x <= 1) || (x >= -10 && x <= -2);
+
+        // Display the result
+        if (belongsToI)
+        {
+            Console.WriteLine($"x belongs to I");
+        }
+        else
+        {
+            Console.WriteLine($"x does not belong to I");
+        }
+    }
+}
